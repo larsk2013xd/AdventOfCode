96e859b5b73678a19be817ff16c7efa2e62bc990
LarsK2013xd <lars.kwant@hotmail.com>
2024-10-31 17:11:50 +0100

    2024 prep

---
+++ 2024/backend.py
@@ -0,0 +1,15 @@
+import time
+def run(function : callable, **kwargs):
+    """Runs a function based on some inputs to get some nicely formatted answers. The function is expected to have as first argument an input file"""
+    t0 = time.perf_counter()
+    result = function(**kwargs)
+    t1 = time.perf_counter()
+    print(f"Completed in {t1 - t0 : .2f} seconds.")
+    print(f"Answer: \n{result}")
+    return result
+
+def test(function : callable, expectedResult, **kwargs):
+    result = run(function, **kwargs)
+    assert result == expectedResult, "Test failed..."
+    print("Test succeeded.")
+
