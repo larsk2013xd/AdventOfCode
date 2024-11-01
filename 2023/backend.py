132593cf8560d704342ada4037fd7f5207c8df53
LarsK2013xd <lars.kwant@hotmail.com>
2024-11-01 16:33:05 +0100

    Start of day 8

--- 2023/backend.py
+++ 2023/backend.py
@@ -1,4 +1,6 @@
 import time
+import os
+import io
 def run(function : callable, **kwargs):
     """Runs a function based on some inputs to get some nicely formatted answers. The function is expected to have as first argument an input file"""
     t0 = time.perf_counter()
@@ -12,4 +14,16 @@ def test(function : callable, expectedResult, **kwargs):
     result = run(function, **kwargs)
     assert result == expectedResult, "Test failed..."
     print("Test succeeded.")
+
+def parseInput(input : str | io.TextIOWrapper, parseFunction = str.splitlines, *args):
+    if type(input) == io.TextIOWrapper :
+        print(f"[Parse of {input.name}]")
+    else:
+        print(f"[Parse]")
+    if type(input) == io.TextIOWrapper : input = input.read()
+    inputElements = parseFunction(input, *args)
+    for inputElement in inputElements[:5]:
+        print(inputElement)
+    print(".\n"*4)
+    return 
 
