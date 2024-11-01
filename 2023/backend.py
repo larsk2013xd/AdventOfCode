9417fae91daf866ae5867ba1452a3f5e0527ee8a
LarsK2013xd <lars.kwant@hotmail.com>
2024-11-01 16:36:24 +0100

    Parser edit

--- 2023/backend.py
+++ 2023/backend.py
@@ -15,15 +15,15 @@ def test(function : callable, expectedResult, **kwargs):
     assert result == expectedResult, "Test failed..."
     print("Test succeeded.")
 
-def parseInput(input : str | io.TextIOWrapper, parseFunction = str.splitlines, *args):
+def parseInput(input : str | io.TextIOWrapper, parseFunction = str.splitlines, printAll = False, *args):
     if type(input) == io.TextIOWrapper :
         print(f"[Parse of {input.name}]")
     else:
         print(f"[Parse]")
     if type(input) == io.TextIOWrapper : input = input.read()
     inputElements = parseFunction(input, *args)
-    for inputElement in inputElements[:5]:
+    for inputElement in inputElements[:5] if not printAll else inputElements:
         print(inputElement)
-    print(".\n"*4)
+    if not printAll : print(".\n"*4)
     return 
 
