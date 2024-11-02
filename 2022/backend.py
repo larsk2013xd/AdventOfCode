6428ef722404f6d899694a9839758fdc2615bab7
LarsK2013xd <lars.kwant@hotmail.com>
2024-11-02 14:46:18 +0100

    2022 part 1

--- 2022/backend.py
+++ 2022/backend.py
@@ -1,28 +1,28 @@
 import time
 import os
 import io
-def run(function : callable, **kwargs):
+def run(function : callable, input):
     """Runs a function based on some inputs to get some nicely formatted answers. The function is expected to have as first argument an input file"""
     t0 = time.perf_counter()
-    result = function(**kwargs)
+    result = function(input)
     t1 = time.perf_counter()
     print(f"Answer: {result}           {(t1-t0) : .6f} seconds")
     return result
 
-def test(function : callable, expectedResult, **kwargs):
-    result = run(function, **kwargs)
+def test(function : callable, input, expectedResult):
+    result = run(function, input)
     assert result == expectedResult, "Test failed..."
     print("Test succeeded.")
 
 def parseInput(input : str | io.TextIOWrapper, parseFunction = str.splitlines, printAll = False, **kwargs):
-    if type(input) == io.TextIOWrapper :
-        print(f"[Parse of {input.name}]")
-    else:
-        print(f"[Parse]")
+    ## Manage input if its a file
     if type(input) == io.TextIOWrapper : input = input.read()
+    print("Puzzle input :")
+    print(input)
+    print('---------------')
+    print("Parsed input: ")
     inputElements = input if not parseFunction else parseFunction(input, **kwargs)
-    for inputElement in inputElements[:5] if not printAll else inputElements:
-        if parseFunction : print(inputElement)
+    print(inputElements)
     if not printAll : print(".\n"*4)
     return inputElements
 
