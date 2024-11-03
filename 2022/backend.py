ee8e0302b28b043afcf3e19721b4f49872844c59
LarsK2013xd <lars.kwant@hotmail.com>
2024-11-03 19:54:50 +0100

    Day 4 complete!

--- 2022/backend.py
+++ 2022/backend.py
@@ -3,10 +3,14 @@ import os
 import io
 import re
 
-
+## Section splitting
 def paragraphs(text : str) : return(text.split("\n\n"))
 def kRows(K : int) : "Creates sections of K rows."; return lambda W : list(W.splitlines()[k*K:(k+1)*K] for k in range(len(W.splitlines())//K))
 lines = str.splitlines
+
+## Parsing
+def parseInts(text : str) -> tuple[int]: return tuple(map(int, re.findall(r"\d+", text)))
+def parseLetters(text : str) -> tuple[str]: return tuple(map(str, re.findall(r"[a-zA-Z]", text)))
 
 def run(function : callable, input):
     """Runs a function based on some inputs to get some nicely formatted answers. The function is expected to have as first argument an input file"""
@@ -21,11 +25,7 @@ def test(function : callable, input, expectedResult):
     assert result == expectedResult, f"Test failed. Expected {expectedResult}, but got {result} instead."
     print("Test succeeded.")
 
-def parseInts(text : str) -> tuple[int]:
-    return tuple(map(int, re.findall(r"\d+", text)))
 
-def parseLetters(text : str) -> tuple[str]:
-    return tuple(map(str, re.findall(r"[a-zA-Z]", text)))
 
 def parseInput(input : str, parseMethod = None, sections = lines):
     print(f"{10*"_"} Input to be parsed {10*"_"}")
