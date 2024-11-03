dc562c295d7525535074ffad0c87e2c020b287fd
LarsK2013xd <lars.kwant@hotmail.com>
2024-11-03 17:34:16 +0100

    Day 3 complete.

--- 2022/backend.py
+++ 2022/backend.py
@@ -2,6 +2,12 @@ import time
 import os
 import io
 import re
+
+
+def paragraphs(text : str) : return(text.split("\n\n"))
+def kRows(K : int) : "Creates sections of K rows."; return lambda W : list(W.splitlines()[k*K:(k+1)*K] for k in range(len(W.splitlines())//K))
+lines = str.splitlines
+
 def run(function : callable, input):
     """Runs a function based on some inputs to get some nicely formatted answers. The function is expected to have as first argument an input file"""
     t0 = time.perf_counter()
@@ -12,7 +18,7 @@ def run(function : callable, input):
 
 def test(function : callable, input, expectedResult):
     result = run(function, input)
-    assert result == expectedResult, "Test failed..."
+    assert result == expectedResult, f"Test failed. Expected {expectedResult}, but got {result} instead."
     print("Test succeeded.")
 
 def parseInts(text : str) -> tuple[int]:
@@ -21,16 +27,13 @@ def parseInts(text : str) -> tuple[int]:
 def parseLetters(text : str) -> tuple[str]:
     return tuple(map(str, re.findall(r"[a-zA-Z]", text)))
 
-def paragraphs(text : str) : return(text.split("\n\n"))
-lines = str.splitlines
-
-def parseInput(input : str, parseMethod = parseInts, sections = lines):
+def parseInput(input : str, parseMethod = None, sections = lines):
     print(f"{10*"_"} Input to be parsed {10*"_"}")
     for line in input.splitlines()[:5] : print(line)
     print("... and maybe more")
     print(20*"_")
     inputSections = sections(input)
-    parsedInputs = list(map(parseMethod, inputSections))
+    parsedInputs = list(map(parseMethod, inputSections)) if parseMethod else inputSections
     print(f"{10*"_"} Parsed input {10*"_"}")
     for parsedInput in parsedInputs[:5] : print(f"{parsedInput}")
     print("... and maybe more")
