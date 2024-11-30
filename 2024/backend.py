18bb93496f629477257e2c076e05f1ef6a26f587
Lars K <lars.kwant@hotmail.com>
2024-11-30 14:07:40 +0100

    Backend ready

--- 2024/backend.py
+++ 2024/backend.py
@@ -1,15 +1,53 @@
 import time
-def run(function : callable, **kwargs):
+import os
+import io
+import re
+
+
+## Section splitting
+def paragraphs(text : str) : return(text.split("\n\n"))
+def kRows(K : int) : "Creates sections of K rows."; return lambda W : list(W.splitlines()[k*K:(k+1)*K] for k in range(len(W.splitlines())//K))
+def columns(text : str) :
+    """Splits the text into rows and returns the columns, assuming there are spaces between each column"""
+    rows = text.splitlines()
+    columns = ["" for _ in range((max(map(len, rows))))] # placeholder
+    for i in range(len(rows)):
+        for j in range(len(rows[i])):
+            columns[j] += rows[i][j]
+    return columns
+lines = str.splitlines
+
+## Parsing
+def parseInts(text : str) -> tuple[int]: return tuple(map(int, re.findall(r"\d+", text)))
+def parseLetters(text : str) -> tuple[str]: return tuple(map(str, re.findall(r"[a-zA-Z]", text)))
+def parseWords(text : str) -> tuple[str] : return tuple(map(str, re.findall(r"[^ ]+", text)))
+
+def run(function : callable, input):
     """Runs a function based on some inputs to get some nicely formatted answers. The function is expected to have as first argument an input file"""
     t0 = time.perf_counter()
-    result = function(**kwargs)
+    result = function(input)
     t1 = time.perf_counter()
-    print(f"Completed in {t1 - t0 : .2f} seconds.")
-    print(f"Answer: \n{result}")
+    print(f"Answer: {result}           {(t1-t0) : .6f} seconds")
     return result
 
-def test(function : callable, expectedResult, **kwargs):
-    result = run(function, **kwargs)
-    assert result == expectedResult, "Test failed..."
+def test(function : callable, input, expectedResult):
+    result = run(function, input)
+    assert result == expectedResult, f"Test failed. Expected {expectedResult}, but got {result} instead."
     print("Test succeeded.")
 
+
+
+def parseInput(input : str, parseMethod = None, sections = lines, ignoreEmpty = False, surpress = False):
+    if not surpress : print(f"{10*"_"} Input to be parsed {10*"_"}")
+    for line in input.splitlines()[:5] : print(line)
+    if not surpress : print("... and maybe more")
+    if not surpress : print(20*"_")
+    inputSections = sections(input)
+    parsedInputs = list(map(parseMethod, inputSections)) if parseMethod else inputSections
+    if ignoreEmpty : parsedInputs = [parsedInput for parsedInput in parsedInputs if len(parsedInput) >= 1]
+    if not surpress : print(f"{10*"_"} Parsed input {10*"_"}")
+    for parsedInput in parsedInputs[:5] : print(f"{parsedInput}")
+    if not surpress : print("... and maybe more")
+    if not surpress : print(20*"_")
+    if len(parsedInputs) == 1 : parsedInputs = parsedInputs[0]
+    return parsedInputs
